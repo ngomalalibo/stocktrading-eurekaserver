f46bf1b9e68be2b7a29a0e50c95f004bcabdaf07
Ngo <ngomalalibo@yahoo.com>
2021-01-22 10:01:27 +0100

    Deployment to AWS EC2 with Travis CI

--- travis_scripts/launch_build.sh
+++ travis_scripts/launch_build.sh
@@ -1,11 +1,11 @@
 sudo apt-get remove docker docker-engine docker.io containerd runc
 sudo apt-get update
-sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 gem install travis
 sudo rm /usr/local/bin/docker-compose
 curl -L https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m) >docker-compose
